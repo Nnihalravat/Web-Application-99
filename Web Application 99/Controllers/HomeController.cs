6dbb8d469619d1e240a3b102d9cd16df660bb24c
Nworks-27 <nihal.naresh.2117@gmail.com>
2024-04-18 15:51:41 +0530

    Add project files.

---
+++ Web Application 99/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Application_99.Controllers
+{
+
+  [ApiController]
+  [Route("[controller]")]
+  public class HomeController : ControllerBase
+  {
+    
+  }
+}
